a45b36d047ea44ac122f24db6fb6768126ff021c
kimotoatsumi <atsumi000105@gmail.com>
2019-02-03 16:18:43 +0530

    edited backend as to User update

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const passport = require('passport');
 const _ = require('lodash');
-const nodemailer=require('nodemailer');
+const nodemailer = require('nodemailer');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
@@ -10,22 +10,24 @@ const User = mongoose.model('User');
 var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: 'hawkeyeitfac@gmail.com',
-      pass: 'moraitfac123'
+        user: 'hawkeyeitfac@gmail.com',
+        pass: 'moraitfac123'
     }
-  });
+});
 module.exports.register = (req, res, next) => {
-    
-    var user=new User({
+
+    var user = new User({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         email: req.body.email,
-        
-        password : req.body.password,
-        age:req.body.age,
-        temptoken:req.body.temptoken
-        });
-        user.save((err, doc) => {
+        user_name: "",
+        studied: "",
+        work_place: "",
+        password: req.body.password,
+        age: req.body.age,
+        temptoken: req.body.temptoken
+    });
+    user.save((err, doc) => {
         if (!err)
             res.send(doc);
         else {
@@ -40,7 +42,7 @@ module.exports.register = (req, res, next) => {
 
 module.exports.authenticate = (req, res, next) => {
     // call for passport authentication
-    passport.authenticate('local', (err, user, info) => {       
+    passport.authenticate('local', (err, user, info) => {
         // error from passport middleware
         if (err) return res.status(400).json(err);
         // registered user
@@ -50,120 +52,152 @@ module.exports.authenticate = (req, res, next) => {
     })(req, res);
 }
 
-module.exports.userProfile = (req, res, next) =>{
+module.exports.userProfile = (req, res, next) => {
     User.findOne({ _id: req._id },
         (err, user) => {
             if (!user)
                 return res.status(404).json({ status: false, message: 'User record not found.' });
-            else{console.log('Im in backend');
-                return res.status(200).json({ status: true, user});}
+            else {
+                console.log('Im in backend');
+                return res.status(200).json({ status: true, user });
+            }
         }
     );
 }
 
 
-module.exports.getname=(req,res,next)=>{
-    User.findOne({email:req.params.email}).select().exec((err,user)=>{
+module.exports.getname = (req, res, next) => {
+    User.findOne({ email: req.params.email }).select().exec((err, user) => {
         console.log(bcrypt.getRounds(user.password))
-            if(!user){ console.log('svdjsdj')
-                res.json({sucsess:false,message:'email was not found'})
-            }
-    
-            else{ 
-                var email={
-                    from:'',
-                    to:'',
-                    subject:user.firstName,
-                    text:bcrypt.getRounds(user.password)
-                };
-                transporter.sendMail(email, function(error, info){
-                    if (error) {
-                      console.log(error);
-                    } else {
-                      console.log('Email sent: ' + info.response);
-                    }
-                  });
-                  return res.status(200).json({ status: true, user});
-            }
-        
-        
+        if (!user) {
+            console.log('svdjsdj')
+            res.json({ sucsess: false, message: 'email was not found' })
+        }
+
+        else {
+            var email = {
+                from: '',
+                to: '',
+                subject: user.firstName,
+                text: bcrypt.getRounds(user.password)
+            };
+            transporter.sendMail(email, function (error, info) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('Email sent: ' + info.response);
+                }
+            });
+            return res.status(200).json({ status: true, user });
+        }
+
+
     })
-    
-    
-    
+
+
+
 
 }
 
-module.exports.puttoken=(req,res)=>{
-    User.findOne({email:req.body.email}).select().exec((err,user)=>{console.log(user)
-        if(err) throw err;
-        if(!user){
-            res.json({sucsess:false,message:'user was not found'})
+module.exports.puttoken = (req, res) => {
+    User.findOne({ email: req.body.email }).select().exec((err, user) => {
+        console.log(user)
+        if (err) throw err;
+        if (!user) {
+            res.json({ sucsess: false, message: 'user was not found' })
         }
-        else{
-            user.temptoken= user.generateJwt();
-            user.save((err)=>{
-                if(err){
-                    res.json({sucsess:false,message:err})
+        else {
+            user.temptoken = user.generateJwt();
+            user.save((err) => {
+                if (err) {
+                    res.json({ sucsess: false, message: err })
                 }
-                else{
-                    var email={
-                        from:'parkheresl@gmail.com',
-                        to:user.email,
-                        subject:user.firstName,
-                        text:'http://localhost:4200/newpassword/'+user.temptoken
+                else {
+                    var email = {
+                        from: 'parkheresl@gmail.com',
+                        to: user.email,
+                        subject: user.firstName,
+                        text: 'http://localhost:4200/newpassword/' + user.temptoken
                     };
-                    transporter.sendMail(email, function(error, info){
+                    transporter.sendMail(email, function (error, info) {
                         if (error) {
-                          console.log(error);
+                            console.log(error);
                         } else {
-                          console.log('Email sent: ' + info.response);
-                          res.json({sucsess:true,message:'message was send to your email'})
+                            console.log('Email sent: ' + info.response);
+                            res.json({ sucsess: true, message: 'message was send to your email' })
                         }
-                      });
+                    });
                 }
             })
         }
     })
 }
 
-module.exports.rstpw=(req,res)=>{
+
+
+
+module.exports.edituser = (req, res) => {
+    console.log("Responce recieved");
+    console.log(JSON.stringify(req.params.id) + " body " + JSON.stringify(req.body.email));
+
+
+
+        var userS = {
+            email: req.body.email,
+            user_name: req.body.user_name,
+            studied: req.body.studied,
+            work_place: req.body.work_place,
+            age: req.body.age
+        };
+
+
+        User.findByIdAndUpdate(req.params.id, { $set: userS }, { new: true }, (err, doc) => {
+            if (!err) { res.send(doc); }
+            else { console.log('Error in User Update :' + JSON.stringify(err, undefined, 2)); }
+        });
+
+    
+};
+
+
+module.exports.rstpw = (req, res) => {
     console.log(req.params.token)
-    User.findOne({ temptoken:req.params.token}).select().exec((err,user)=>{
+    User.findOne({ temptoken: req.params.token }).select().exec((err, user) => {
         console.log(user)
-        if(err) throw err;
-        var token=req.params.token;
-        jwt.verify(token,process.env.JWT_SECRET,(err,decoded)=>{
-            if(err){
-                res.json({sucsess:false,message:'password link has expired'});
-            }else{
+        if (err) throw err;
+        var token = req.params.token;
+        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+            if (err) {
+                res.json({ sucsess: false, message: 'password link has expired' });
+            } else {
                 console.log(user)
-                res.json({sucsess:true,user:user});
+                res.json({ sucsess: true, user: user });
 
             }
         })
     })
 }
 
-module.exports.savepassword=(req,res)=>{console.log(req.body)
-    User.findOne({email:req.body.email}).select().exec((err,user)=>{
+module.exports.savepassword = (req, res) => {
+    console.log(req.body)
+    User.findOne({ email: req.body.email }).select().exec((err, user) => {
         console.log(req.body.newpassword)
-        if(err) throw err;
-        if(req.body.newpassword==null||req.body.newpassword==''){
-            res.json({sucsess:false,message:'Password not provided'});
-        }   
-        else{
-            user.password=req.body.newpassword;
-            user.temptoken='';
-            user.save((err)=>{
-                if(err){
-                    res.json({sucsess:false,message:err})
+        if (err) throw err;
+        if (req.body.newpassword == null || req.body.newpassword == '') {
+            res.json({ sucsess: false, message: 'Password not provided' });
+        }
+        else {
+            user.password = req.body.newpassword;
+            user.temptoken = '';
+            user.save((err) => {
+                if (err) {
+                    res.json({ sucsess: false, message: err })
                 }
-                else{
-                    res.json({sucsess:true,message:'Password has been reset'});
+                else {
+                    res.json({ sucsess: true, message: 'Password has been reset' });
                 }
             })
-            
+
         }
     })
 
